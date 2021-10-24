835e34b23134350837cde2a3cc30f4d5fd06e4a7
thisisanash <khananash28@gmail.com>
2021-10-24 11:43:40 +0530

    Portfolio Internal Section Complete

--- script.js
+++ script.js
@@ -16,25 +16,10 @@ var $grid = $('.gallery-grid').isotope({
     }
 });
 
-//-- filter functions
-// var filterFns = {
-//     // show if number is greater than 50
-//     numberGreaterThan50: function () {
-//         var number = $(this).find('.number').text();
-//         return parseInt(number, 10) > 50;
-//     },
-//     // show if name ends with -ium
-//     ium: function () {
-//         var name = $(this).find('.name').text();
-//         return name.match(/ium$/);
-//     }
-//-- };
-
 // bind filter button click
 $('#filters').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
     // use filterFn if matches value
-    // -- filterValue = filterFns[filterValue] || filterValue;
     $grid.isotope({ filter: filterValue });
 });
 
@@ -47,11 +32,8 @@ $('.filter-group').each(function (i, buttonGroup) {
     });
 });
 
+// Initiate portfolio lightbox 
+const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+});
 
-// for layout 
-
-// $('.gallery-grid').isotope({
-//     layoutMode: 'fitColumns',
-//     itemSelector: '.element-item'
-//   });
-  

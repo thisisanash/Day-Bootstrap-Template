ac7a2dc945761a7341d74cc22ae046e13dbf1c68
thisisanash <khananash28@gmail.com>
2021-10-28 15:22:39 +0530

    Navbar staky position done

--- script.js
+++ script.js
@@ -1,39 +1,124 @@
-// external js: isotope.pkgd.js
+(function () {
+    // external js: isotope.pkgd.js
 
-// init Isotope
-var $grid = $('.gallery-grid').isotope({
-    layoutMode: 'packery',
-    itemSelector: '.element-item',
-    getSortData: {
-        name: '.name',
-        symbol: '.symbol',
-        number: '.number parseInt',
-        category: '[data-category]',
-        weight: function (itemElem) {
-            var weight = $(itemElem).find('.weight').text();
-            return parseFloat(weight.replace(/[\(\)]/g, ''));
+    // init Isotope
+    var $grid = $('.gallery-grid').isotope({
+        layoutMode: 'packery',
+        itemSelector: '.element-item',
+        getSortData: {
+            name: '.name',
+            symbol: '.symbol',
+            number: '.number parseInt',
+            category: '[data-category]',
+            weight: function (itemElem) {
+                var weight = $(itemElem).find('.weight').text();
+                return parseFloat(weight.replace(/[\(\)]/g, ''));
+            }
+        }
+    });
+
+    // bind filter button click
+    $('#filters').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        // use filterFn if matches value
+        $grid.isotope({ filter: filterValue });
+    });
+
+    // change is-checked class on buttons
+    $('.filter-group').each(function (i, buttonGroup) {
+        var $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'button', function () {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
+    });
+
+    // Initiate portfolio lightbox 
+    const portfolioLightbox = GLightbox({
+        selector: '.portfolio-lightbox'
+    });
+
+
+    // make function to select section easy
+    /**
+    * Easy selector helper function
+    */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
         }
     }
-});
 
-// bind filter button click
-$('#filters').on('click', 'button', function () {
-    var filterValue = $(this).attr('data-filter');
-    // use filterFn if matches value
-    $grid.isotope({ filter: filterValue });
-});
 
-// change is-checked class on buttons
-$('.filter-group').each(function (i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function () {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-    });
-});
 
-// Initiate portfolio lightbox 
-const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-});
+    //scroll and other function
 
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+        let header = select('#navbar')
+        let offset = header.offsetHeight
+
+        if (!header.classList.contains('header-scrolled')) {
+            offset -= 16
+        }
+
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+        })
+    }
+
+
+    /**
+     * Header fixed top on scroll
+     */
+    let selectHeader = select('#navbar')
+    if (selectHeader) {
+        let headerOffset = selectHeader.offsetTop
+        // let nextElement = selectHeader.nextElementSibling
+        let nextElement = select(".hero")
+        const headerFixed = () => {
+            if ((headerOffset - window.scrollY) <= 0) {
+                selectHeader.classList.add('fixed-top')
+                nextElement.classList.add('scrolled-offset')
+            } else {
+                selectHeader.classList.remove('fixed-top')
+                nextElement.classList.remove('scrolled-offset')
+            }
+        }
+        window.addEventListener('load', headerFixed)
+        onscroll(document, headerFixed)
+    }
+
+})()

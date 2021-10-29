ddfa3875a1d9bcedf0dda967394e842f0a9b8909
thisisanash <khananash28@gmail.com>
2021-10-29 23:05:07 +0530

    color and dropdown correction

--- script.js
+++ script.js
@@ -121,4 +121,17 @@
         onscroll(document, headerFixed)
     }
 
+
+    /**
+     * aos animation
+     */
+    AOS.init();
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
 })()

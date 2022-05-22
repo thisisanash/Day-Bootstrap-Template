e82458609828f8ca7ac172e1b8d2554dd7acb063
Muhammad Anash <55632429+thisisanash@users.noreply.github.com>
2022-05-22 17:24:47 +0530

    minor bugs fixed - > scroll bar style > hover elements

--- script.js
+++ script.js
@@ -121,6 +121,26 @@
         onscroll(document, headerFixed)
     }
 
+    /**
+     * Navbar links active state on scroll
+     * */
+    let navbarlinks = select('#navbar .nav-link', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 110
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
 
     /**
      * aos animation
